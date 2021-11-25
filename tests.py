020b929004c774bb8222c8af1b73df3710bdee56
dshzhkv <dashunich.f@gmail.com>
2021-11-25 12:10:53 +0500

    tests and coverage

--- tests.py
+++ tests.py
@@ -1,8 +1,8 @@
 from unittest import TestCase
-from game.BallGenerator import BallGenerator
-from game.Path import Path
-from game.Sprites import *
-from game.ShootingManager import ShootingManager
+from BallGenerator import BallGenerator
+from Path import Path
+from Sprites import *
+from ShootingManager import ShootingManager
 
 
 def are_lists_equal(expected, actual):
